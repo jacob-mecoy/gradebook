4a83e041fc4676ee64bd5b6f07a8b0022c513ebc
jacob-mecoy <jacob.mecoy@gmail.com>
2022-02-06 20:10:48 +0000

    Edited Writeline so that it prints a variable passed when running the application

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿//Our application is a console application i.e. it is run from the command line
+using System;
 
 namespace GradeBook
 {
     class Program
     {
+        //entry point of our application. When running the program this is what is executed
+        //our methods parameter is an array of strings called 'args'
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //set up a conditional statement that checks whether or not an argument is
+            //passed when running the program
+            if(args.Length > 0)
+            { 
+                //string interpolation used in the writeline method
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            { 
+                //if there is no argument then we will default to the message below
+                Console.WriteLine("Hello!");
+            }
+
+            
         }
     }
 }
---
+++ src/GradeBook/Requirements.md
@@ -0,0 +1,6 @@
+#We need an electronic grade book to read some scores
+#of an individual student and then compute some simple
+#statistics from the scores.
+#The grades are entered as floating point numbers from
+#0 to 100, and the statistics should show us the highest
+#grade, the lowest grade, and the average grade
