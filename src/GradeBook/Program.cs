a363e2c9db00b44606e761d54f5527395a41d77e
jacob-mecoy <jacob.mecoy@gmail.com>
2022-02-06 21:14:10 +0000

    Added a variable that is the sum of two doubles and writes variable to console.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,6 +9,16 @@ namespace GradeBook
         //our methods parameter is an array of strings called 'args'
         static void Main(string[] args)
         {
+            //define a variable of type double and instantiate it by assigning a value to it
+            double x = 34.1;
+            //we can use the var keyword when defining a variable and initialising it
+            //on the same line. When using the var keyword we take advantage of implicit
+            //typing in C#. The compiler figures out the type of our variable.
+            var y = 90.1;
+            var z = x + y;
+
+            Console.WriteLine($"The result of our addition is {z}");
+
             //set up a conditional statement that checks whether or not an argument is
             //passed when running the program
             if(args.Length > 0)
